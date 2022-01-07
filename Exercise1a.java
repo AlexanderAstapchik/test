4792dc3922ed99d3459b0d8bca200b9f4586af8f
Alexander <astapchik.alex1502@gmail.com>
2022-01-07 18:32:05 +0200

    initial commit

---
+++ Exercise1a.java
@@ -0,0 +1,11 @@
+public class Exercise1a {
+public static void main (String[] args) {
+int a = Integer.valueOf(args[0]);
+int x=1;
+while (a/10!=0) {
+a=a/10;
+x++;
+}
+System.out.println (x);
+}
+}
